a0d555be227bc7f10972fe83d5a0caa725f60a98
omoola <omoola410@gmail.com>
2024-03-18 10:03:23 +0100

    added styled to my input

--- src/ui/StyledInput.tsx
+++ src/ui/StyledInput.tsx
@@ -3,11 +3,12 @@ import styled from "styled-components";
 export const StyledInput = styled.input`
   padding: 0.3rem 0.5rem;
   background-color: var(--input-bg);
-  border-radius: 1px;
+  border-radius: 8px;
   width: 100%;
   margin-bottom: 0.25rem;
   outline: none;
   color: var(--color-text-sec);
   font-size: 0.65rem;
-  box-shadow: 0 2px 4px rgba(0, 0, 0);
+  /* box-shadow: 0 2px 4px rgba(0, 0, 0); */
+  box-shadow: 0 1px 6px 0 rgba(0, 0, 0,.5);
 `;
